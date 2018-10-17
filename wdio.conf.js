ba020d75ec947269ec5f75410a5fc8d6ac517a95
Ali Hill <alastair.hill@ecs-digital.co.uk>
2018-10-17 15:53:58 +0100

    Adding allure into config

--- wdio.conf.js
+++ wdio.conf.js
@@ -9,6 +9,13 @@ exports.config = {
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
+    reporters: ['dot', 'allure'],
+    reporterOptions: {
+        allure: {
+            outputDir: 'allure-results'
+        }
+    },
+    
     specs: [
         './test/*.js'
     ],
