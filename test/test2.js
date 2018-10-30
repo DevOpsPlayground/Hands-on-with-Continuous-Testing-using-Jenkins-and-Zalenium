039c5ebf2971cb02858f001f474c8daa48605a8b
AliHill <43469419+AliHill@users.noreply.github.com>
2018-10-30 11:36:10 +0000

    test2.js back to google

--- test/test2.js
+++ test/test2.js
@@ -1,9 +1,9 @@
 var assert = require('assert');
-describe('my webdriverio tests', function(){
-	it('Github test',function() {
- 		browser.url('https://ecs-digital.co.uk/careers')
+describe('Google Test Suite', function(){
+	it('Google Test 1',function() {
+ 		browser.url('http://www.google.com')
  		const title = browser.getTitle();
  		console.log('Title was: ' + title);
- 		assert(title === 'ECS Digital – Your career within the world of digital transformation');
+ 		assert(title === 'Google');
  	});
 });
