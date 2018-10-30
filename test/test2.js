054cf298b958d294fdd6fbd0ed82dace333f55ce
AliHill <43469419+AliHill@users.noreply.github.com>
2018-10-30 11:58:06 +0000

    test2 update

--- test/test2.js
+++ test/test2.js
@@ -4,6 +4,7 @@ describe('my webdriverio tests', function(){
  		browser.url('https://ecs-digital.co.uk/careers')
  		const title = browser.getTitle();
  		console.log('Title was: ' + title);
+		browser.pause(2000);
  		assert(title === 'ECS Digital – Your career within the world of digital transformation');
  	});
 });
