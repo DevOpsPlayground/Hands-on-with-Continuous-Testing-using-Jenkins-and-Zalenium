021259b939cb81239231839e42c50e541d311adb
AliHill <43469419+AliHill@users.noreply.github.com>
2018-10-30 11:16:19 +0000

    update test2

--- test/test2.js
+++ test/test2.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 describe('my webdriverio tests', function(){
 	it('Github test',function() {
- 		browser.url('https://ecs-digital.co.uk/careers')
+ 		browser.url('https://ecs-digital.co.uk')
  		const title = browser.getTitle();
  		console.log('Title was: ' + title);
  		assert(title === 'Careers');
