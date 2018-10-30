d2d5f82908e4b8a1e67767e64bc64cacda31d0a5
AliHill <43469419+AliHill@users.noreply.github.com>
2018-10-30 11:39:57 +0000

    blank test8

--- test/test8.js
+++ test/test8.js
@@ -1,9 +1,1 @@
-var assert = require('assert');
-describe('Google Test Suite', function(){
-	it('Google Test 8',function() {
- 		browser.url('http://www.google.com')
- 		const title = browser.getTitle();
- 		console.log('Title was: ' + title);
- 		assert(title === 'Google');
- 	});
-});
+
