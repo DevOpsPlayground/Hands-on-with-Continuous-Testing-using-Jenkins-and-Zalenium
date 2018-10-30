83f9598f6ddac987c980e08617d6df9db38a2712
AliHill <43469419+AliHill@users.noreply.github.com>
2018-10-30 11:47:36 +0000

    test.js update

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ describe('my webdriverio tests', function(){
  		browser.url('https://ecs-digital.co.uk/')
  		const title = browser.getTitle();
  		console.log('Title was: ' + title);
- 		assert(title === 'ECS');
+ 		assert(title === 'ECS Digital – Digital Transformation Consultancy');
  	});
 });
