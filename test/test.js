b5caf0968b3519e89d489e826e732d80d3829e77
AliHill <43469419+AliHill@users.noreply.github.com>
2018-10-30 11:42:52 +0000

    test.js update

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var assert = require('assert');
-describe('Google Test Suite', function(){
-	it('Google Test 1',function() {
- 		browser.url('http://www.google.com')
+describe('my webdriverio tests', function(){
+	it('Github test',function() {
+ 		browser.url('https://ecs-digital.co.uk/')
  		const title = browser.getTitle();
  		console.log('Title was: ' + title);
- 		assert(title === 'Google');
+ 		assert(title === 'ECS');
  	});
 });
