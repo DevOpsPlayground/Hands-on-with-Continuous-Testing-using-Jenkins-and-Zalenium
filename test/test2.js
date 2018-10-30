13ab5417e28a2f90cef7afe99754fab93f28df2a
AliHill <43469419+AliHill@users.noreply.github.com>
2018-10-30 11:20:21 +0000

    test2.js update

--- test/test2.js
+++ test/test2.js
@@ -1,9 +1,9 @@
 var assert = require('assert');
 describe('my webdriverio tests', function(){
 	it('Github test',function() {
- 		browser.url('https://ecs-digital.co.uk')
+ 		browser.url('https://ecs-digital.co.uk/careers')
  		const title = browser.getTitle();
  		console.log('Title was: ' + title);
- 		assert(title === 'Careers');
+ 		assert(title === 'ECS Digital – Your career within the world of digital transformation');
  	});
 });
